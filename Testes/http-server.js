dc615b1facda39ca0a1ec919caef1d67d0a4aac7
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-06-06 18:09:18 +0000

    Alegria

--- Testes/http-server.js
+++ Testes/http-server.js
@@ -4,10 +4,30 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.use("/qhrssao", (req, res) => {
+app.use("/create", (req, res) => { //Criar
   const { file, texto } = req.query
   fs.writeFileSync(file, texto)
-  res.send(new Date())
+  res.send(`<div>Criado</div>`)
+})
+
+app.use("/read", (req, res) => { //Ler
+  const { file } = req.query
+  const texto = fs.readFileSync(file)
+  res.send(texto.toString()) //Converte em 'String' e imprime
+})
+
+app.use("/update", (req, res) => { //Atualizar
+  const { file, texto } = req.query
+  fs.appendFileSync(file, texto)
+  res.send()
+})
+
+app.use("/delete", (req, res) => { //Deletar
+  const { file } = req.query
+  fs.rmSync(file)
+  res.send("Deletado")
 })
 
 app.listen(8080, () => console.log("Servidor rodando!"))
+
+//update?file=Alegria&texto=nnnnnnn | Para alterar
