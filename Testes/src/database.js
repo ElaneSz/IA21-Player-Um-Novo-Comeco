dfd30561656d4dad74f1b01425d2daa7df7b41fc
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-06-13 18:07:45 +0000

    Rebeca- 13.06 Tarde

--- Testes/src/database.js
+++ Testes/src/database.js
@@ -4,26 +4,27 @@ const { open } =  require('sqlite')
 let instance = null
 
 async function  getDatabaseInstance() { /*'export' possibilita que ele seja acessado por outro arquivo*/
-    if (!instance)
+    if (instance)
         return instance
 
-    const dataBase = await open ({
+    const database = await open ({
         filename: 'database.sqlite' ,
         driver: sqlite3.Database
     })
 
     await database.exec(`
-    CREAT TABLE IF NOT EXISTS movies (
-        id  INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        source TEXT NOT NULL,
-        description TEXT,
-        thumb TEXT
-    );
+        CREATE TABLE IF NOT EXISTS movies (
+            id  INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            source TEXT NOT NULL,
+            description TEXT,
+            thumb TEXT
+        );
     `)
 
-    instance = dataBase
+    instance = database
+
     return database
 }
 
-module.export = { getDatabaseInstance }
+module.exports = { getDatabaseInstance }
--- Testes/src/index.js
+++ Testes/src/index.js
@@ -9,29 +9,30 @@ app.use(express.static(__dirname + '/public'))
 app.use("/create", async (req, res) => { // Criar
   const { title, source, description, thumb } = req.query
   const db = await getDatabaseInstance() // Conexão com o BD
-  const resut = await bd.run(`
-    INSERT INTO movies(title, source, description, thumb) VALUES(?, ?, ?, ?)`,
-    [title, source, description, thumb]
-  )
+  const resut = await db.run(`INSERT INTO movies(title, source, description, thumb) VALUES(?, ?, ?, ?)`, [title, source, description, thumb])
   res.send(resut)
 })
 
-app.use("/read", (req, res) => { // Ler
-  const { file } = req.query
-  const texto = fs.readFileSync(file)
-  res.send(texto.toString()) // Converte em 'String' e imprime
+app.use("/read", async (req, res) => { // Ler
+  const { id } = req.query
+  const db = await getDatabaseInstance()
+  const resut = await db.get(`SELECT * FROM movies WHERE id=?`, [id])
+  //res.send(id.toString()) // Converte em 'String' e imprime
+  res.send(resut)
 })
 
-app.use("/update", (req, res) => { // Atualizar
-  const { file, texto } = req.query
-  fs.appendFileSync(file, texto)
-  res.send("Atualizado!!")
+app.use("/update", async (req, res) => { // Atualizar
+  const { id, title, source, description, thumb } = req.query
+  const db = await getDatabaseInstance()
+  const resut = await db.run(`UPDATE movies SET title=?, source=?, description=?, thumb=? WHERE id=?`, [title, source, description, thumb, id])
+  res.send(resut)
 })
 
-app.use("/delete", (req, res) => { // Deletar
-  const { file } = req.query
-  fs.rmSync(file)
-  res.send("Deletado!!")
+app.use("/delete", async (req, res) => { // Deletar
+  const { id } = req.query
+  const db = await getDatabaseInstance()
+  const resut = await db.run(`DELETE FROM movies WHERE id=?`, [id])
+  res.send(resut)
 })
 
 app.listen(3000, () => console.log("Servidor rodando!")) // node http-server.js | Para executar o servidor
