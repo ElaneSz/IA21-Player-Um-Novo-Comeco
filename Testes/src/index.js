49d2faf0ce6a60ba869402318953a0ab80a1c1b5
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-06-27 20:16:42 +0000

    27.06 | Tarde

--- Testes/src/index.js
+++ Testes/src/index.js
@@ -5,7 +5,7 @@ const { log } = require("console")
 
 const app = express()
 
-app.use(express.static(__dirname + '/public')) /*Caso não ache a rota | Pega o arquivo do jeito que é e envia para o cliente*/
+app.use(express.static(__dirname + '/../public')) /*Caso não ache a rota | Pega o arquivo do jeito que é e envia para o cliente*/
 app.use(express.json()) /*Converte o que veio, em .json*/
 
 app.post("/movies", async (req, res) => { // Criar
@@ -17,14 +17,14 @@ app.post("/movies", async (req, res) => { // Criar
 })
 
 app.get("/movies", async (req, res) => { // Ler
-  //const { id } = req.query
+  const { id } = req.query
   const db = await getDatabaseInstance()
-  /*if (id) {
+  if (id) {
     const result = await db.get(`SELECT * FROM movies WHERE id=?`, id)
     res.json(result)
     return
-  }*/
-  const result = await db.all(`SELECT * FROM movies`)
+  }
+  const result = await db.all(`SELECT * FROM movies ORDER BY id DESC`)
   res.json(result)
   //res.json(id.toString()) // Converte em 'String' e imprime
   //res.json(resut) /*'.json' mostra o cabeçalho completo*/
@@ -35,10 +35,11 @@ app.get("/movies/:id", async (req, res) => { // Ler
   const db = await getDatabaseInstance()
   const result = await db.get(`SELECT * FROM movies WHERE id=?`, id)
   res.json(result)
+  return
 })
 
-app.put("/movies", async (req, res) => { // Atualizar
-  const { id } = req.query
+app.put("/movies/:id", async (req, res) => { // Atualizar
+  const { id } = req.params
   const { title, source, description, thumb } = req.body
   const db = await getDatabaseInstance()
   const resut = await db.run(`UPDATE movies SET title=?, source=?, description=?, thumb=? WHERE id=?`, 
@@ -47,8 +48,9 @@ app.put("/movies", async (req, res) => { // Atualizar
 })
 
 /*==========================================================*/
-app.patch("/movies", async (req, res) => { /*Atualizar somente um*/
-  const { id } = req.query
+
+app.patch("/movies/:id", async (req, res) => { /*Atualizar somente um*/
+  const { id } = req.params
   const db = await getDatabaseInstance()
   const info = Object.keys(req.body).map(key => `${key}=?` ).join(', ') /*Percorre os valores e organiza o lado esquerdo*/
   const values = [...Object.values(req.body), id] /* Copia e adiciona o array para o array raiz | Quebra e adiciona o 'id' | Em um array só com valores*/
@@ -65,11 +67,17 @@ app.patch("/movies", async (req, res) => { /*Atualizar somente um*/
 
 //Object.keys(x).map(key => `${key}=?` ).join(', ')
 
-app.delete("/movies", async (req, res) => { // Deletar
+/*app.delete("/movies", async (req, res) => { // Deletar
   const { id } = req.query
   const db = await getDatabaseInstance()
-  const resut = await db.run(`DELETE FROM movies WHERE id=?`, 
-  [id])
+  const resut = await db.run(`DELETE FROM movies WHERE id=?`,  [id])
+  res.json(resut) /*'.json' mostra o cabeçalho completo
+})*/
+
+app.delete("/movies/:id", async (req, res) => { // Deletar
+  const { id } = req.params
+  const db = await getDatabaseInstance()
+  const resut = await db.run(`DELETE FROM movies WHERE id=?`,  [id])
   res.json(resut) /*'.json' mostra o cabeçalho completo*/
 })
 
