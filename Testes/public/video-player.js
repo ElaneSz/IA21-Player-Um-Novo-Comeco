becc75b114725ee11caf49dd55a091bffe3a45e8
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-08-06 01:58:10 +0000

    Caixa de dialogo atualizada

--- Testes/public/video-player.js
+++ Testes/public/video-player.js
@@ -30,13 +30,7 @@ const containers = document.querySelectorAll("div.ia21-player")/*'querySelectorA
             
             lista.forEach(filme => {
                 playlist.innerHTML += `
-                <div>${filme.title}
-                <ul>
-                    <li><strong>Source: </strong>${filme.source}</li>
-                    <li><strong>Description: </strong>${filme.description}</li>
-                    <li><strong>Thumb: </strong>${filme.thumb}</li>
-                </ul>
-                </div>
+                <div>${filme.title}</div>
                 `
             })
             
